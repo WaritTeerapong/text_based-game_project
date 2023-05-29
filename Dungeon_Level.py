65ef1ab04af0ac2d5f4711581108ed0174d30c52
WaritTeerapong <warit.teer@kmutt.ac.th>
2023-05-29 22:12:35 +0700

    Update Dungeon_Level.py
    + main Dungeon sequence (algorithm)
    + Abstract level function

--- Dungeon_Level.py
+++ Dungeon_Level.py
@@ -1,12 +1,54 @@
 from Steve import *
 
 exit_dungeon = False
+level = 0
+
+def lv_1():
+    pass
+
+def lv_2():
+    pass
+
+def lv_3():
+    pass
+
+def lv_4():
+    pass
+
+
+def next_level(current_level):
+    ore_chance = 0
+    mobs_chance = 0
+    
+    
+    
+    pass
 
 # main sequence
 def dungeon_scene():
     while exit_dungeon == False:
-        pass
-    """
-    if go exploring deeper || back to village
-    monster and ore rate
-    """
+        if level>0:
+            print("Would you like to go deeper or back to village")
+            print("1) continue exploring")
+            print("2) back to village")
+            choice = input(">> ")
+            
+            choice = "0" 
+            while choice not in ["1","2"]:
+                choice = input(">> ")
+                if choice == "1" :
+                    level += 1 
+                    break
+                elif choice == "2":
+                    input("you are exiting dungeon...")
+                    exit_dungeon = True
+                    
+        #exit dungeon
+        if exit_dungeon: 
+            continue
+                    
+        next_level(level)
+              
+        
+        
+        
